68f94b916fa7cc5c1d3c6bf29a0d18b7f239055f
mounikay06 <47276249+mounikay06@users.noreply.github.com>
2019-08-09 13:08:40 +0530

    Add files via upload

---
+++ Mounika-Swish-hackathon.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int discount,price;
+	int total=0;
+	printf("enter price and discount\n");
+	scanf("%d %d",&price,&discount);
+
+	while(price>0){
+
+		total+=price;
+		price = (100-discount)*price/100;
+
+	}
+	printf("%d",total);
+	return 0;
+}
